261b34c97f9043ddacc98cb1e1846d0de29d7069
Alanes Gomes <alanesGsilva@gmail.com>
2026-07-04 01:15:28 +0000

    Nivel iniciante concluido

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+public class Main { //criando classe MAIN//
+    public static void main(String[] args) { //criando método para classe main//
+        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5); //criando objeto - heroi 1//
 
-        .
-        .
-        .
+        heroi1.exibirStatus(); //chamando o método e exibindo saída para heroi 1//
+
+        Personagem heroi2 = new Personagem("Ártemis", "Arqueira", 5, 80, 12.5); // criando objeto - heroi 2 //
+
+        heroi2.exibirStatus(); //chamando o método e exibindo a saída para heroi 2//
 
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,23 +1,27 @@
-public class Personagem {
+public class Personagem { //criando classe "Personagem" e seus atributos
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) { //utilizando construtor para facilidar 
         this.nome = nome;
-        .
-        .
-        .
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
 
     }
 
-    public void exibirStatus() {
+    public void exibirStatus() { //método "exibirStatus" e saídas.
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
+        System.out.println();
 
     }
 }
